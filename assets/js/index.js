334c509dee258ebf6ac2a50bb2a7e3583891cc87
James Bond <57707960+rcavalero@users.noreply.github.com>
2020-01-02 18:24:31 -0800

    create initial files.

---
+++ assets/js/index.js
@@ -0,0 +1,60 @@
+
+// Objective:
+    // GIVEN the developer has a GitHub profile
+    // WHEN prompted for the developer's GitHub username and favorite color
+    // THEN a PDF profile is generatedThe PDF will be populated with the following:
+
+    // Profile image
+    // User name
+    // Links to the following:
+    // User location via Google Maps
+    // User GitHub profile
+    // User blog
+    // User bio
+    // Number of public repositories
+    // Number of followers
+    // Number of GitHub stars
+    // Number of users following
+
+
+// Minimum Requirements
+    // Functional, deployed application.
+    // GitHub repository with a unique name and a README describing project.
+    // The application generates a PDF resume from the user provided GitHub profile.
+    // The generated resume includes a bio image from the user's GitHub profile.
+    // The generated resume includes the user's location and a link to their GitHub profile.
+    // The generated resume includes the number of: public repositories, followers, GitHub stars and following count.
+    // The background color of the generated PDF matches the color that the user provides.
+
+// Submission Requirements
+    // You are required to submit the following:
+    // An animated GIF demonstrating the app functionality
+    // A generated PDF of your GitHub profile
+    // The URL of the GitHub repository
+
+
+// *** Todo List  ***
+
+// write code to extract above data from github
+    // need prompts for username and favorite color
+// save results to an html file  (look at today's exercise)
+// convert html file to a pdf file (Clint published something in slack)
+// create animated gif of the app functionality
+// save a pdf of my profile
+// create README file 
+
+
+
+const questions = [
+
+    
+
+];
+
+function writeToFile(fileName, data) {
+ 
+}
+
+function init() {
+
+init();
