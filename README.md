8d107bb4e510e6070f9162ff4cb97fd8c98dfebf
James Bond <57707960+rcavalero@users.noreply.github.com>
2020-01-06 11:32:57 -0800

    added code to save html as pdf document.

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 # GitHub Portfolio Generator
 
 
-This application dynamically creates an HTML file using data from a GitHub user profile and converts the result to a PDf file.  A script file is run usine  NodeJS that prompts the user for a GitHub username and a color choice.  Data is then extracted from GitHub and an HTML file and PDF documents are created using the user's color choice in the design of the document.
-
-The application is located here: [Profile Generator](https://rcavalero.github.io/githubprofilegenerator/) 
+This application dynamically creates an HTML file using data from a GitHub user profile and converts the result to a PDf file.  A script file is run using  NodeJS that prompts the user for a GitHub username and a color choice.  Data is then extracted from GitHub and an HTML file and PDF documents are created using the user's color choice in the design of the document.
 
 The product of this application is seen here:
 
--- assets/js/index.js
+++ assets/js/index.js
@@ -11,9 +11,7 @@
 // *** Todo List  ***
 
 // revise code to use variables and functions in sample code
-// convert html file to a pdf file (Clint published something in slack)
 // create animated gif of the app functionality
-
 
 // *** coding starts here ***
 const fs = require("fs");
@@ -23,25 +21,17 @@ const inquirer = require("inquirer");
 const generateHTML = require("./generateHTML.js");
 const writeFileAsync = util.promisify(fs.writeFile);
 const gs = require('github-scraper');
+const pdfcrowd = require("pdfcrowd");
+
 
 let data = {};
 
+
 async function getData() {
+// this prompts the user for username and color choice then retreives data using a couple of queiries against github APIs
     try {
         const userAnswers = await
             inquirer.prompt(questions);
-                // {
-                //     type: "input",
-                //     message: "Enter your GitHub username",
-                //     name: "username"
-                // },
-                // {
-                //     type: "list",
-                //     message: "What is your favorite color?",
-                //     choices: ["green", "blue", "pink", "red"],
-                //     name: "color"
-                // }
-            // ]);
 
         const queryUrl = `https://api.github.com/users/${userAnswers.username}`;
         const gitData = await axios.get(queryUrl);
@@ -64,9 +54,33 @@ async function getData() {
                 stars: scraperData.stars,
                 following: scraperData.following
             }
+
+            // this creates the index.html  file using the above data the format from generateHTML.js
         const html = generateHTML.generateHTML(data);
-        writeFileAsync("index.html", html);
-            }); 
+        writeFileAsync("../../index.html", html);
+
+        // this creates a PDF document using the html string above
+        var client = new pdfcrowd.HtmlToPdfClient("rcavalero", "bbfee87a0727daaf33389b6a07838172");
+        try {
+            client.setPageWidth("8.27in");
+            client.setScaleFactor(75)
+            client.setPrintPageRange("1")
+        } catch(why) {
+            console.error("Pdfcrowd Error: " + why);
+            console.error("Pdfcrowd Error Code: " + why.getCode());
+            console.error("Pdfcrowd Error Message: " + why.getMessage());
+            process.exit(1);
+        }
+
+        client.convertStringToFile(
+            html,
+            "../images/profile.pdf",
+            function(err, fileName) {
+                if (err) return console.error("Pdfcrowd Error: " + err);
+                console.log("Success: the file was created " + fileName);
+                
+            });
+        }); 
     }
         catch (err) {
             console.log(err);
